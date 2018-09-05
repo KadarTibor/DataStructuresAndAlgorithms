ae11ce0ef33295ae1ef72bfefc85aa802990bf42
KadarTibor <kadartibor24@gmail.com>
2018-09-05 21:34:32 +0300

    implements method that rotates matrix with 90 degrees in place

--- java-DSA/src/main/java/strings/ArrayUtils.java
+++ java-DSA/src/main/java/strings/ArrayUtils.java
@@ -16,19 +16,39 @@ public class ArrayUtils {
         for(int i = 0; i < input.length / 2; i++) {
             int layer = i;
             for(int j = layer; j < input.length - layer; j++) {
-                //save up-left value
-                int topLeft = input[layer][input.length-layer];
+                //save top-left value
+                int topLeft = input[layer][j];
+                //save top-right value
+                int topRight = input[j][input.length - 1 - layer];
+                //save bottom-right value
+                int bottomRight = input[input.length - 1 - layer][input.length - 1 - j];
+                //save bottom-left value
+                int bottomLeft = input[input.length - 1 - j][layer];
 
                 //move top-right to bottom-right
-                input[input.length - layer][input.length - layer] = input[layer][input.length-layer];
+                input[input.length - 1 - layer][input.length - 1 - j] = topRight;
 
-                //move up-left to up-right
-                input[layer][input.length-layer] = input[layer][j];
+                //move bottom-right to bottom-left
+                input[input.length - 1 - j][layer] = bottomRight;
 
-                //move up
+                //move bottom-left to top-left
+                input[layer][j] = bottomLeft;
+
+                //move top-left to top-right
+                input[layer + j][input.length - 1 - layer] = topLeft;
             }
         }
 
         return input;
+    }
+
+
+    public void printMatrix(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
---
+++ java-DSA/src/main/java/strings/ArrayUtilsTestCase.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class ArrayUtilsTestCase {
+
+    public static void main(String[] args) {
+        ArrayUtils arrayUtils = new ArrayUtils();
+
+        int[][] matrix = {{3,1,3},{3,1,3},{3,1,3}};
+
+        testMatrixRotation(arrayUtils, matrix);
+    }
+
+    public static void testMatrixRotation(ArrayUtils arrayUtils,int[][] matrix) {
+        arrayUtils.printMatrix(matrix);
+        arrayUtils.printMatrix(arrayUtils.rotateMatrix(matrix));
+    }
+}
