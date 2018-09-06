4b2896925e444fcd1b77fb957b3755bd1fff0a34
KadarTibor <kadartibor24@gmail.com>
2018-09-06 20:46:47 +0300

    implements the zeroing out algorithm

--- java-DSA/src/main/java/arrays/ArrayUtils.java
+++ java-DSA/src/main/java/arrays/ArrayUtils.java
@@ -1,9 +1,12 @@
 package arrays;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ArrayUtils {
 
 
-    public ArrayUtils(){
+    public ArrayUtils() {
 
     }
 
@@ -11,11 +14,11 @@ public class ArrayUtils {
      * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
      * write a method to rotate the image by 90 degrees. Can you do this in place?
      */
-    public int[][] rotateMatrix(int[][] input){
+    public int[][] rotateMatrix(int[][] input) {
 
-        for(int i = 0; i < input.length / 2; i++) {
+        for (int i = 0; i < input.length / 2; i++) {
             int layer = i;
-            for(int j = layer; j < input.length - layer; j++) {
+            for (int j = layer; j < input.length - layer; j++) {
                 //save top-left value
                 int topLeft = input[layer][j];
                 //save top-right value
@@ -43,9 +46,51 @@ public class ArrayUtils {
     }
 
 
+    /**
+     * Write an algorithm such that if an element in an MxN matrix is 0, its entire row and column is set to 0.
+     *
+     * @param matrix
+     */
+    public int[][] zeroOutMatrix(int[][] matrix) {
+
+        Set<Integer> columns = new HashSet<>();
+        Set<Integer> rows = new HashSet<>();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    columns.add(j);
+                    matrix = clearRowsAndColumns(matrix, i, j);
+                }
+
+                if (columns.contains(j) || rows.contains(i)) {
+                    matrix[i][j] = 0;
+                }
+
+            }
+        }
+
+        return matrix;
+    }
+
+    private int[][] clearRowsAndColumns(int[][] matrix, int row, int column) {
+
+        for (int i = 0; i < row; i++) {
+            matrix[i][column] = 0;
+        }
+
+        for (int i = 0; i < column; i++) {
+            matrix[column][i] = 0;
+        }
+
+        return matrix;
+    }
+
+
     public void printMatrix(int[][] matrix) {
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix.length; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
--- java-DSA/src/main/java/arrays/ArrayUtilsTestCase.java
+++ java-DSA/src/main/java/arrays/ArrayUtilsTestCase.java
@@ -5,13 +5,22 @@ public class ArrayUtilsTestCase {
     public static void main(String[] args) {
         ArrayUtils arrayUtils = new ArrayUtils();
 
-        int[][] matrix = {{3,1,3},{3,1,3},{3,1,3}};
+        int[][] matrix = {{3, 1, 3}, {3, 0, 3}, {3, 1, 3}};
 
         testMatrixRotation(arrayUtils, matrix);
+
+        testZeroOutMatrix(arrayUtils, matrix);
     }
 
-    public static void testMatrixRotation(ArrayUtils arrayUtils,int[][] matrix) {
+    public static void testMatrixRotation(ArrayUtils arrayUtils, int[][] matrix) {
+        System.out.println("\nTest Matrix Rotation");
         arrayUtils.printMatrix(matrix);
         arrayUtils.printMatrix(arrayUtils.rotateMatrix(matrix));
+    }
+
+    public static void testZeroOutMatrix(ArrayUtils arrayUtils, int[][] matrix) {
+        System.out.println("\nTest zeroing out matrix");
+        arrayUtils.printMatrix(matrix);
+        arrayUtils.printMatrix(arrayUtils.zeroOutMatrix(matrix));
     }
 }
