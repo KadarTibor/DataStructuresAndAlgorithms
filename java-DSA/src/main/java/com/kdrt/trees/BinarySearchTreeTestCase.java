afde76595846c497fdc3cac353d0e7b8f35bf06b
KadarTibor <kadartibor24@gmail.com>
2018-09-10 15:59:16 +0300

    adds tree implementation and parseing

---
+++ java-DSA/src/main/java/com/kdrt/trees/BinarySearchTreeTestCase.java
@@ -0,0 +1,18 @@
+package com.kdrt.trees;
+
+public class BinarySearchTreeTestCase {
+
+    public static void main(String[] args) {
+        BinarySearchTree bs = new BinarySearchTree();
+
+        Node root = new Node(new Node(new Node(10),11, new Node(13)),15, new Node(new Node(new Node(17), 18, new Node(19)), 20, new Node(21)));
+
+        bs.printInorder(root, 0);
+
+        if(bs.checkBalanced(root)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+}
